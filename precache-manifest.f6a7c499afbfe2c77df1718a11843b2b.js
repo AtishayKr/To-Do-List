30d82593c1087858281819c9ae1e4d44ea20366c
atishay kumar <atishay1950@gmail.com>
2022-11-21 12:19:52 +0530

    Updates

---
+++ precache-manifest.f6a7c499afbfe2c77df1718a11843b2b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd90c62159b85f5ff9f74cbda0f9158b",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "07a5a1e9956efdaa9fc6",
+    "url": "/to-do-list/static/js/2.83133fa3.chunk.js"
+  },
+  {
+    "revision": "9a44fe82f4f5af060918",
+    "url": "/to-do-list/static/js/main.51a15190.chunk.js"
+  },
+  {
+    "revision": "3ddb58e12b77e51cacc4",
+    "url": "/to-do-list/static/js/runtime-main.dca1c00e.js"
+  }
+]);
